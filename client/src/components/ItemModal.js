54ad8dd040e013e5c67ee63baf668e18561f9d48
lamecksilva <lameck2@hotmail.com>
2018-10-19 23:10:48 -0300

    Ainda falta terminar com o redux

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ItemModal extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,7 +3,7 @@ import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
 import { connect } from "react-redux";
-import { getItems } from "../actions/itemActions";
+import { getItems, deleteItem } from "../actions/itemActions";
 import PropTypes from "prop-types";
 
 class ShoppingList extends Component {
@@ -11,6 +11,9 @@ class ShoppingList extends Component {
     this.props.getItems();
   }
 
+  onDeleteclick = id => {
+    this.props.deleteItem(id);
+  };
   render() {
     const { items } = this.props.item;
 
@@ -40,11 +43,7 @@ class ShoppingList extends Component {
                     className="remove-btn"
                     color="danger"
                     size="sm"
-                    onClick={() => {
-                      this.setState(state => ({
-                        items: state.items.filter(item => item.id !== id)
-                      }));
-                    }}
+                    onClick={this.onDeleteclick.bind(this, id)}
                   >
                     &times;
                   </Button>
@@ -70,5 +69,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getItems }
+  { getItems, deleteItem }
 )(ShoppingList);
